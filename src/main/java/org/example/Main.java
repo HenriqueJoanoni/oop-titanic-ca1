9f6d03613d4efe619b9001f86d8955f4d370bd46
HenriqueJoanoni <henrique.joanoni@gmail.com>
2024-10-09 19:29:26 +0100

    build(Project): java solutions

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,364 @@
+package org.example;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        String fileName = "titanic-data-100.csv"; // file should be in the project folder (below pom.xml)
+
+        ArrayList<Passenger> passengerList = new ArrayList<>();
+
+        loadPassengerDataFromFile(passengerList, fileName);
+
+//        displayAllPassengers( passengerList );
+
+        // Assignment: Implement and test the following methods
+
+        /** GET PASSENGER NAMES */
+        System.out.println("PASSENGER NAMES : " + Arrays.toString(getPassengerNames(passengerList)));
+
+        /** GET PASSENGER CONTAINING NAMES */
+        System.out.println("PASSENGER CONTAINING NAMES : " + getPassengersContainingNames(passengerList, "William"));
+
+        /** PASSENGERS ELDER THAN */
+        System.out.println("PASSENGER ELDER THAN 20: " + getPassengersOlderThan(passengerList, 20));
+
+        /** COUNT BY GENDER */
+        System.out.println("ONLY MALE PASSENGERS: " + countPassengersByGender(passengerList, "male"));
+        System.out.println("ONLY FEMALE PASSENGERS: " + countPassengersByGender(passengerList, "female"));
+
+        /** SUM FARES */
+        System.out.printf("THE SUM OF ALL THE FARES IS: [$%.2f]\r\n", sumFares(passengerList));
+
+        /** COUNT OF MALE SURVIVORS */
+        System.out.println("NAME OF MALE SURVIVORS: " + Arrays.toString(maleSurvivors(passengerList)));
+
+        /** TICKET OWNER */
+        System.out.println("PASSENGER TICKET NUMBER: " + ticketOwner(passengerList, "123"));
+
+        /** AVERAGE AGE */
+        System.out.println("AVERAGE AGE: " + averageAge(passengerList));
+
+        /** PASSENGER BY TICKET CLASS */
+        System.out.println("PASSENGERS INTO A CERTAIN CLASS : " + getPassengersByTicketClass(passengerList, PassengerClass.FIRST));
+
+        /** SORTING BY ID */
+        sortPassengersByPassengerId(passengerList);
+        for (Passenger passenger : passengerList) {
+            System.out.println(passenger);
+        }
+
+        /** SORT PASSENGERS BY NAME */
+        sortPassengersByName(passengerList);
+        for (Passenger passenger : passengerList) {
+            System.out.println(passenger);
+        }
+
+        /** SORT PASSENGERS BY AGE AND NAME */
+        List<Passenger> sortedByAgeThenName = sortPassengersByAgeThenName(passengerList);
+
+        for (Passenger passenger : sortedByAgeThenName) {
+            System.out.println(passenger);
+        }
+
+        /** SORT PASSENGERS BY GENDER AND PASSENGER NUMBER */
+        List<Passenger> sortedByGenderThenNumber = sortPassengersByGenderThenPassengerNumber(passengerList);
+
+        for (Passenger passenger : sortedByGenderThenNumber) {
+            System.out.println(passenger);
+        }
+
+        /** SORTED BY FARE THEN SURVIVAL */
+        List<Passenger> sortedByFareThenSurvival = sortPassengersByFareThenSurvival(passengerList);
+
+        for (Passenger passenger : sortedByFareThenSurvival) {
+            System.out.println(passenger);
+        }
+
+        /** SORTED BY TICKET CLASS */
+        List<Passenger> sortedByTicketClass = sortPassengersByTicketClass(passengerList);
+
+        for (Passenger passenger : sortedByTicketClass) {
+            System.out.println(passenger);
+        }
+
+        /** SORTED BY AGE */
+        List<Passenger> sortedByAge = sortPassengersByAge(passengerList);
+
+        for (Passenger passenger : sortedByAge) {
+            System.out.println(passenger);
+        }
+
+        /** SORTED BY TICKET NUMBER LAMBDA */
+        List<Passenger> sortedByTicketNumberLambda = sortPassengersByTicketNumberLambda(passengerList);
+
+        for (Passenger passenger : sortedByTicketNumberLambda) {
+            System.out.println(passenger);
+        }
+
+        /** SORTED BY NUMBER STATIC  */
+        List<Passenger> sortedByTicketNumberStatic = sortPassengersByTicketNumberStatic(passengerList);
+
+        for (Passenger passenger : sortedByTicketNumberStatic) {
+            System.out.println(passenger);
+        }
+
+        /** FIND PASSENGER BY TICKET NUMBER */
+        Collections.sort(passengerList, Passenger.TicketNumberComparator);
+
+        Passenger targetPassengerByTicketNumber = new Passenger(null, "B456");
+        Passenger foundPassengerByTicketNumber = findPassengerByTicketNumber(passengerList, targetPassengerByTicketNumber);
+
+        if (foundPassengerByTicketNumber != null) {
+            System.out.println("Passenger found! " + foundPassengerByTicketNumber);
+        } else {
+            System.out.println("Passenger not found :(");
+        }
+
+        /** FIND PASSENGER BY ID */
+        Collections.sort(passengerList, Passenger.PassengerIdComparator);
+
+        Passenger targerPassengerById = new Passenger("2", null);
+
+        Passenger foundPassengerById = findPassengerByPassengerId(passengerList, targerPassengerById);
+
+        if (foundPassengerById != null) {
+            System.out.println("Passenger found! " + foundPassengerById);
+        } else {
+            System.out.println("Passenger not found :(");
+        }
+
+    }
+
+    /**
+     * CUSTOM METHODS
+     */
+
+    public static String[] getPassengerNames(ArrayList<Passenger> passengerList) {
+        String[] passengerNames = new String[passengerList.size()];
+
+        for (int i = 0; i < passengerList.size(); i++) {
+            passengerNames[i] = passengerList.get(i).getName();
+        }
+
+        return passengerNames;
+    }
+
+    public static List<Passenger> getPassengersContainingNames(List<Passenger> passengerList, String entryName) {
+        List<Passenger> passengersContainingNames = new ArrayList<>();
+
+        for (Passenger passenger : passengerList) {
+            if (passenger.getName().contains(entryName)) {
+                passengersContainingNames.add(passenger);
+            }
+        }
+
+        return passengersContainingNames;
+    }
+
+    public static List<Passenger> getPassengersOlderThan(List<Passenger> passengerList, int age) {
+        List<Passenger> passengersOlderThan = new ArrayList<>();
+
+        for (Passenger passenger : passengerList) {
+            if (passenger.getAge() > age) {
+                passengersOlderThan.add(passenger);
+            }
+        }
+
+        return passengersOlderThan;
+    }
+
+    public static List<Passenger> countPassengersByGender(List<Passenger> passengerList, String gender) {
+        List<Passenger> passengersByGender = new ArrayList<>();
+
+        for (Passenger passenger : passengerList) {
+            if (passenger.getGender().equals(gender)) {
+                passengersByGender.add(passenger);
+            }
+        }
+
+        return passengersByGender;
+    }
+
+    public static double sumFares(ArrayList<Passenger> passengerList) {
+        double totalFares = 0.0;
+        for (Passenger passenger : passengerList) {
+            totalFares += passenger.getFare();
+        }
+
+        return totalFares;
+    }
+
+    public static String[] maleSurvivors(List<Passenger> passengerList) {
+        String[] maleSurvivors = new String[passengerList.size()];
+
+        for (int i = 0; i < passengerList.size(); i++) {
+            if (passengerList.get(i).getGender().equals("male")) {
+                maleSurvivors[i] = passengerList.get(i).getName();
+            }
+        }
+
+        return maleSurvivors;
+    }
+
+    public static Passenger ticketOwner(ArrayList<Passenger> passengerList, String ticketNumber) {
+        for (Passenger passenger : passengerList) {
+            if (passenger.getTicketNumber().equals(ticketNumber)) {
+                return passenger;
+            }
+        }
+        return null;
+    }
+
+    public static int averageAge(ArrayList<Passenger> passengerList) {
+        int totalAge = 0;
+
+        for (Passenger passenger : passengerList) {
+            totalAge += passenger.getAge();
+        }
+
+        return totalAge / passengerList.size();
+    }
+
+    public static ArrayList<Passenger> getPassengersByTicketClass(ArrayList<Passenger> passengerList, PassengerClass passengerClass) {
+        ArrayList<Passenger> passengersByTicketClass = new ArrayList<>();
+        for (Passenger passenger : passengerList) {
+            if (passenger.getPassengerClass() == passengerClass) {
+                passengersByTicketClass.add(passenger);
+            }
+        }
+        return passengersByTicketClass;
+    }
+
+    public static void sortPassengersByPassengerId(ArrayList<Passenger> passengerList) {
+        passengerList.sort(Comparator.comparing(Passenger::getPassengerId));
+    }
+
+    public static void sortPassengersByName(ArrayList<Passenger> passengerList) {
+        passengerList.sort(Comparator.comparing(Passenger::getName));
+    }
+
+    public static List<Passenger> sortPassengersByAgeThenName(List<Passenger> passengerList) {
+        passengerList.sort(Comparator.comparing(Passenger::getAge).thenComparing(Passenger::getName));
+        return passengerList;
+    }
+
+    public static List<Passenger> sortPassengersByGenderThenPassengerNumber(List<Passenger> passengerList) {
+        passengerList.sort(Comparator.comparing(Passenger::getGender).thenComparing(Passenger::getPassengerId));
+        return passengerList;
+    }
+
+    public static List<Passenger> sortPassengersByFareThenSurvival(List<Passenger> passengerList) {
+        passengerList.sort(Comparator.comparing(Passenger::getFare).thenComparing(Comparator.comparing(Passenger::getSurvived).reversed()));
+        return passengerList;
+    }
+
+    public static List<Passenger> sortPassengersByTicketClass(List<Passenger> passengerList) {
+        passengerList.sort(Comparator.comparing(Passenger::getTicketClass));
+        return passengerList;
+    }
+
+    public static List<Passenger> sortPassengersByAge(List<Passenger> passengerList) {
+        passengerList.sort(new Comparator<Passenger>() {
+            @Override
+            public int compare(Passenger p1, Passenger p2) {
+                return p1.getAge().compareTo(p2.getAge());
+            }
+        });
+        return passengerList;
+    }
+
+    public static List<Passenger> sortPassengersByTicketNumberLambda(List<Passenger> passengerList) {
+        passengerList.sort((p1, p2) -> p1.getTicketNumber().compareTo(p2.getTicketNumber()));
+        return passengerList;
+    }
+
+    public static List<Passenger> sortPassengersByTicketNumberStatic(List<Passenger> passengerList) {
+        passengerList.sort(Passenger.TicketNumberComparator);
+        return passengerList;
+    }
+
+    public static Passenger findPassengerByTicketNumber(List<Passenger> passengerList, Passenger targetPassenger) {
+        Collections.sort(passengerList, Passenger.TicketNumberComparator);
+
+        int index = Collections.binarySearch(passengerList, targetPassenger, Passenger.TicketNumberComparator);
+
+        if (index >= 0) {
+            return passengerList.get(index);
+        } else {
+            return null;
+        }
+    }
+
+    public static Passenger findPassengerByPassengerId(List<Passenger> passengerList, Passenger targetPassenger) {
+        Collections.sort(passengerList, Passenger.PassengerIdComparator);
+
+        int index = Collections.binarySearch(passengerList, targetPassenger, Passenger.PassengerIdComparator);
+
+        if (index >= 0) {
+            return passengerList.get(index);
+        } else {
+            return null;
+        }
+    }
+
+    public static void loadPassengerDataFromFile(ArrayList<Passenger> passengerList, String fileName) {
+
+        // Format of each row of data is:
+        // Name,Age,Height(m),GPA  - these heading names are included as the first row in file
+        // John Malone,20,1.78,3.55   for example
+
+        // Use a Regular Expression to set both comma and newline as delimiters.
+        //  sc.useDelimiter("[,\\r\\n]+");
+        // Text files in windows have lines ending with "CR-LF" or "\r\n" sequences.
+        // or may have only a newline - "\n"
+        // Windows uses CRLF (\r\n, 0D 0A) line endings while Unix just uses LF (\n, 0A).
+        //
+        try (Scanner sc = new Scanner(new File(fileName))
+                .useDelimiter("[,\\r\\n]+")) {
+
+            // skip past the first line, as it has field names (not data)
+            if (sc.hasNextLine())
+                sc.nextLine();   // read the header line containing column titles, but don't use it
+
+            // while there is a next token to read....
+            System.out.println("Go...");
+
+            while (sc.hasNext()) {
+                String passengerId = sc.next();    // read passenger ID
+                int survived = sc.nextInt();     // 0=false, 1=true
+                int passengerClass = sc.nextInt();  // passenger class, 1=1st, 2=2nd or 3rd
+                String name = sc.next();
+                String gender = sc.next();
+                int age = sc.nextInt();
+                int siblingsAndSpouses = sc.nextInt();
+                int parentsAndChildren = sc.nextInt();
+                String ticketNumber = sc.next();
+                double fare = sc.nextDouble();
+                String cabin = sc.next();
+                String embarkedAt = sc.next();
+                /** ADDING NEW ATTRIBUTE HERE  */
+                Integer ticketClass = sc.nextInt();
+
+                System.out.println(passengerId + ", " + name);
+
+                passengerList.add(
+                        new Passenger(passengerId, survived, passengerClass,
+                                name, gender, age, siblingsAndSpouses, parentsAndChildren, ticketNumber,
+                                fare, cabin, embarkedAt, ticketClass) // NEW ATTRIBUTE ticketClass
+                );
+            }
+        } catch (FileNotFoundException exception) {
+            System.out.println("FileNotFoundException caught. The file " + fileName + " may not exist." + exception);
+        }
+    }
+
+    public static void displayAllPassengers(ArrayList<Passenger> passengerList) {
+        for (Passenger passenger : passengerList) {
+            System.out.println(passenger);
+        }
+    }
+}
---
+++ src/main/java/org/example/Passenger.java
@@ -0,0 +1,209 @@
+package org.example;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Passenger implements Comparable<Passenger> {
+    private String passengerId;    // passenger number
+    private int survived;           // 0=false, 1=true
+    private PassengerClass passengerClass;  // passenger class, 1=1st, 2=2nd or 3rd class
+    private String name;
+    private String gender;
+    private int age;
+    private int siblingsAndSpouses;    // number of
+    private int parentsAndChildren;     // number of
+    private String ticketNumber;
+    private double fare;                // cost of ticket
+    private String cabin;           // cabin, list of cabins or NoCabin
+    private String embarkedAt;      // port where passenger boarded ship
+    /** NEW ATTRIBUTE */
+    private Integer ticketClass;
+
+    public Passenger(String passengerId, int survived, int passengerClassAsNumber, String name,
+                     String gender, int age, int siblingsAndSpouses, int parentsAndChildren,
+                     String ticketNumber, double fare, String cabin, String embarkedAt, Integer ticketClass) {
+        this.passengerId = passengerId;
+        this.survived = survived;
+        this.ticketClass = ticketClass;
+
+        if (passengerClassAsNumber == 1) {
+            passengerClass = PassengerClass.FIRST;
+        } else if (passengerClassAsNumber == 2) {
+            passengerClass = PassengerClass.SECOND;
+        } else if (passengerClassAsNumber == 3) {
+            passengerClass = PassengerClass.THIRD;
+        } else if (passengerClassAsNumber == 4) {
+            passengerClass = PassengerClass.UNKNOWN;
+        }
+
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.siblingsAndSpouses = siblingsAndSpouses;
+        this.parentsAndChildren = parentsAndChildren;
+        this.ticketNumber = ticketNumber;
+        this.fare = fare;
+        this.cabin = cabin;
+        this.embarkedAt = embarkedAt;
+    }
+
+    /** OVERLOADED CONSTRUCTOR FOR SORTING BY PASSENGER ID AND TICKET NUMBER */
+    public Passenger(String passengerId, String ticketNumber) {
+        this.passengerId = passengerId;
+        this.ticketNumber = ticketNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger{" +
+                "passengerId='" + passengerId + '\'' +
+                ", survived=" + survived +
+                ", passengerClass=" + passengerClass +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                ", siblingsAndSpouses=" + siblingsAndSpouses +
+                ", parentsAndChildren=" + parentsAndChildren +
+                ", ticketNumber=" + ticketNumber +
+                ", fare=" + fare +
+                ", cabin='" + cabin + '\'' +
+                ", embarkedAt='" + embarkedAt + '\'' +
+                '}';
+    }
+
+    public String getPassengerId() {
+        return passengerId;
+    }
+
+    public void setPassengerId(String passengerId) {
+        this.passengerId = passengerId;
+    }
+
+    public int getSurvived() {
+        return survived;
+    }
+
+    public void setSurvived(int survived) {
+        this.survived = survived;
+    }
+
+    public PassengerClass getPassengerClass() {
+        return passengerClass;
+    }
+
+    public void setPassengerClass(PassengerClass passengerClass) {
+        this.passengerClass = passengerClass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSiblingsAndSpouses() {
+        return siblingsAndSpouses;
+    }
+
+    public void setSiblingsAndSpouses(int siblingsAndSpouses) {
+        this.siblingsAndSpouses = siblingsAndSpouses;
+    }
+
+    public int getParentsAndChildren() {
+        return parentsAndChildren;
+    }
+
+    public void setParentsAndChildren(int parentsAndChildren) {
+        this.parentsAndChildren = parentsAndChildren;
+    }
+
+    public String getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(String ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public double getFare() {
+        return fare;
+    }
+
+    public void setFare(double fare) {
+        this.fare = fare;
+    }
+
+    public String getHasCabin() {
+        return cabin;
+    }
+
+    public void setHasCabin(String hasCabin) {
+        this.cabin = hasCabin;
+    }
+
+    public String getEmbarkedAt() {
+        return embarkedAt;
+    }
+
+    public void setEmbarkedAt(String embarkedAt) {
+        this.embarkedAt = embarkedAt;
+    }
+
+    /** NEW GETTERS AND SETTERS */
+    public Integer getTicketClass() {
+        return ticketClass;
+    }
+
+    public void setTicketClass(Integer ticketClass) {
+        this.ticketClass = ticketClass;
+    }
+
+    public String getCabin() {
+        return cabin;
+    }
+
+    public void setCabin(String cabin) {
+        this.cabin = cabin;
+    }
+
+    /**
+     * COMPARE TO METHOD
+     */
+    @Override
+    public int compareTo(Passenger other) {
+        return this.passengerId.compareTo(other.passengerId);
+    }
+
+    public static final Comparator<Passenger> TicketNumberComparator = new Comparator<Passenger>() {
+        @Override
+        public int compare(Passenger p1, Passenger p2) {
+            return p1.getTicketNumber().compareTo(p2.getTicketNumber());
+        }
+    };
+
+    public static final Comparator<Passenger> PassengerIdComparator = new Comparator<Passenger>() {
+        @Override
+        public int compare(Passenger p1, Passenger p2) {
+            return p1.getPassengerId().compareTo(p2.getPassengerId());
+        }
+    };
+}
